d2fadc5a641b47c29efea65fcae2fccc70276521
Vishwas Dixit <vishwasbkt001@gmail.com>
2025-01-04 15:22:28 +0530

    Jobify all page str...

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomeLayout from './pages/HomeLayout';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomeLayout/>
+  },
+  {
+    path:"/about",
+    element:(
+      <div>
+        <h1>
+          About page
+        </h1>
+      </div>
+    )
+  }
+])
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App
